98e4a5476269846ea28267b44a38e814f84006de
Harry Wu <hanxiangwu86@gmail.com>
2018-03-07 21:42:34 -0800

    added class inheritance

--- magic_methods.php
+++ magic_methods.php
@@ -3,16 +3,16 @@ class Post{
 	private $name;
 
 	public function __set($name, $value){
-		echo 'Setting '.$name.' to <strong>'.$value.'</strong><br />';
+		echo "Setting {$name} to <strong> {$value} </strong><br />";
 		$this->name = $value;
 	}
 
 	public function __get($name){
-		echo 'Getting '.$name.' <strong>'.$this->name.'</strong><br />';
+		echo "Getting {$name} <strong> {$this->name} </strong><br />";
 	}
 
 	public function __isset($name){
-		echo 'Is '.$name.' set?<br />';
+		echo "Is {$name} set? <br />";
 		return isset($this->name);
 	}
 }
