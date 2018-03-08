e714f72354b95485a2fcd28af9129d99ba875737
Harry Wu <hanxiangwu86@gmail.com>
2018-03-07 23:05:08 -0800

    added sql autoload

---
+++ cat.php
@@ -0,0 +1,6 @@
+<?php
+class Cat extends dog{
+	public function sayHello(){
+		echo 'Maooo from Cat';
+	}
+}
---
+++ dog.php
@@ -0,0 +1,6 @@
+<?php
+final class Dog{
+	public function sayHello(){
+		echo 'Wow';
+	}
+}
