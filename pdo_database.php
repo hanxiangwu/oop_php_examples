4441553bf1db413c030c8a46c50673dc6b3b819f
Harry Wu <hanxiangwu86@gmail.com>
2018-03-08 23:11:13 -0800

    added update and delete data

--- pdo_database.php
+++ pdo_database.php
@@ -5,6 +5,13 @@ $database = new Database;
 
 $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
+if($_POST['delete']){
+	$delete_id = $_POST['delete_id'];
+	$database->query('DELETE FROM posts WHERE id = :id');
+	$database->bind(':id', $delete_id);
+	$database->execute();
+}
+
 if($post['submit']){
 	$title = $post['title'];
 	$body = $post['body'];
@@ -16,6 +23,18 @@ if($post['submit']){
 	if($database->lastInsertId()){
 		echo '<p>Post Added!</p>';
 	}
+}
+
+if($post['update']){
+	$id = $post['id'];
+	$title = $post['title'];
+	$body = $post['body'];
+
+	$database->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+	$database->bind(':title', $title);
+	$database->bind(':body', $body);
+	$database->bind(':id', $id);
+	$database->execute();
 }
 
 $database->query('SELECT * FROM posts');
@@ -30,12 +49,28 @@ $rows = $database->resultset();
 	<input type="submit" name="submit" value="Submit" />
 </form>
 
+<h1>update Post</h1>
+<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+	<label>Post ID</label><br />
+	<input type="text" name="id" placeholder="Specify ID" /><br /><br />
+	<label>Post Title</label><br />
+	<input type="text" name="title" placeholder="Add a Title..." /><br /><br />
+	<label>Post Body</label><br />
+	<textarea name="body"></textarea><br /><br />
+	<input type="submit" name="update" value="Update" />
+</form>
+
 <h1>Posts</h1>
 <div>
 <?php foreach($rows as $row) : ?>
 	<div>
 		<h3><?php echo $row['title']; ?></h3>
 		<p><?php echo $row['body']; ?></p>
+		<br />
+		<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+			<input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">
+			<input type="submit" name="delete" value="Delete" />
+		</form>
 	</div>
 <?php endforeach; ?>
 </div>
